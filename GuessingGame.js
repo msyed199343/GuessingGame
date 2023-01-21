e4245838a4a0a3a1987df058e4489f76217db0ad
SyedMohiuddin <msyed199343@gmail.com>
2023-01-21 02:06:19 -0600

    initial

---
+++ GuessingGame.js
@@ -0,0 +1,24 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+
+  rl.question("What's up, doc? ", handleResponseOne);
+
+  function handleResponseOne(firstAnswer) {
+    console.log(firstAnswer + " is up.");
+    rl.question("What's down, clown? ", handleResponseTwo);
+  }
+
+  function handleResponseTwo(secondAnswer) {
+    console.log(secondAnswer + " is down.");
+    rl.question("What's left, Jeff? ", handleResponseThree);
+  }
+
+  function handleResponseThree(thirdAnswer) {
+    console.log(thirdAnswer + " is left.");
+    rl.close();
+  }
